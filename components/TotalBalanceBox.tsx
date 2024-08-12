e2c60904269f1769a01c340dbbc6eb56d8e60442
João Sá <joao.vsaa90@gmail.com>
2024-08-12 16:05:19 -0300

    chore: applies corrections to types

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({
-    accounts= [], totalBanks, totalCurrentBalance}:TotlaBalanceBoxProps)=> {
+    accounts= [], totalBanks, totalCurrentBalance}:TotalBalanceBoxProps)=> {
         return (
             <section className='total-balance'>
                 <div className='total-balance-chart'>
-                    {/* DoughnutChart */}
+                    <DoughnutChart accounts={accounts} />
                 </div>
 
                 <div className='flex flex-col gap-6'>
